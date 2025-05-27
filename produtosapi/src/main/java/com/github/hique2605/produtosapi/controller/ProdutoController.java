7ed1d0035985601e06c7a142fa4a276392e61be8
Henrique Milioli Ronchi <hiquemilioli@gmail.com>
2025-05-27 15:50:05 -0300

    Persistindo produtos no banco de dados

--- produtosapi/src/main/java/com/github/hique2605/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/com/github/hique2605/produtosapi/controller/ProdutoController.java
@@ -1,18 +1,32 @@
 package com.github.hique2605.produtosapi.controller;
 
 import com.github.hique2605.produtosapi.model.Produto;
+import com.github.hique2605.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController     //anotacao diz que essa classe vai receber requisicoes rest.
 @RequestMapping("produtos")  //qual é a url base
 public class ProdutoController {
 
+    private ProdutoRepository produtoRepository;
+    //nao iria precisa do construtor se usa lombok ou usa @Autowired private ProdutoRepository produtoRepository;
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping
     public Produto salvar(@RequestBody Produto produto){
         System.out.println("Produto recebido: " + produto);
+
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+
+        produtoRepository.save(produto);
         return produto;
     }
 
