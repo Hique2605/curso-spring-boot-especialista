a266e53192a06163a25fdf3ec96be72881b00bd3
Henrique Milioli Ronchi <hiquemilioli@gmail.com>
2025-05-27 14:48:41 -0300

    Proejto Criado

---
+++ produtosapi/src/main/java/com/github/hique2605/produtosapi/controller/ProdutoController.java
@@ -0,0 +1,19 @@
+package com.github.hique2605.produtosapi.controller;
+
+import com.github.hique2605.produtosapi.model.Produto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController     //anotacao diz que essa classe vai receber requisicoes rest
+@RequestMapping("produtos")  //qual é a url base
+public class ProdutoController {
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto){
+        System.out.println("Produto recebido: " + produto);
+        return produto;
+    }
+
+}
